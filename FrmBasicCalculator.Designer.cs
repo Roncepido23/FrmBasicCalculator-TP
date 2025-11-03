ee708f59846645fda1d85ee695c1edcba506babb
Roncepido23 <oncepidorobin34@gmail.com>
2025-11-03 06:29:01 -0800

    Malapit naaaa

--- FrmBasicCalculator.Designer.cs
+++ FrmBasicCalculator.Designer.cs
@@ -41,7 +41,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Impact", 30F);
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(124, 26);
+            this.label1.Location = new System.Drawing.Point(14, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(290, 48);
             this.label1.TabIndex = 0;
@@ -50,18 +50,18 @@
             // tbInput1
             // 
             this.tbInput1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbInput1.Location = new System.Drawing.Point(22, 98);
+            this.tbInput1.Location = new System.Drawing.Point(34, 78);
             this.tbInput1.Name = "tbInput1";
-            this.tbInput1.Size = new System.Drawing.Size(430, 44);
+            this.tbInput1.Size = new System.Drawing.Size(242, 44);
             this.tbInput1.TabIndex = 1;
             this.tbInput1.Text = "0";
             // 
             // tbInput2
             // 
             this.tbInput2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbInput2.Location = new System.Drawing.Point(12, 290);
+            this.tbInput2.Location = new System.Drawing.Point(34, 201);
             this.tbInput2.Name = "tbInput2";
-            this.tbInput2.Size = new System.Drawing.Size(430, 44);
+            this.tbInput2.Size = new System.Drawing.Size(242, 44);
             this.tbInput2.TabIndex = 2;
             this.tbInput2.Text = "0";
             // 
@@ -69,7 +69,7 @@
             // 
             this.cbOperation.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbOperation.FormattingEnabled = true;
-            this.cbOperation.Location = new System.Drawing.Point(22, 190);
+            this.cbOperation.Location = new System.Drawing.Point(101, 137);
             this.cbOperation.Name = "cbOperation";
             this.cbOperation.Size = new System.Drawing.Size(99, 45);
             this.cbOperation.TabIndex = 3;
@@ -80,9 +80,9 @@
             this.btn_Compute.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_Compute.Font = new System.Drawing.Font("Impact", 15F);
             this.btn_Compute.ForeColor = System.Drawing.Color.White;
-            this.btn_Compute.Location = new System.Drawing.Point(344, 425);
+            this.btn_Compute.Location = new System.Drawing.Point(80, 385);
             this.btn_Compute.Name = "btn_Compute";
-            this.btn_Compute.Size = new System.Drawing.Size(222, 46);
+            this.btn_Compute.Size = new System.Drawing.Size(138, 46);
             this.btn_Compute.TabIndex = 4;
             this.btn_Compute.Text = "Compute";
             this.btn_Compute.UseVisualStyleBackColor = false;
@@ -91,9 +91,9 @@
             // rtbTotal
             // 
             this.rtbTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rtbTotal.Location = new System.Drawing.Point(579, 170);
+            this.rtbTotal.Location = new System.Drawing.Point(34, 285);
             this.rtbTotal.Name = "rtbTotal";
-            this.rtbTotal.Size = new System.Drawing.Size(430, 96);
+            this.rtbTotal.Size = new System.Drawing.Size(242, 66);
             this.rtbTotal.TabIndex = 5;
             this.rtbTotal.Text = "";
             // 
@@ -102,7 +102,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SlateGray;
-            this.ClientSize = new System.Drawing.Size(1201, 505);
+            this.ClientSize = new System.Drawing.Size(313, 460);
             this.Controls.Add(this.rtbTotal);
             this.Controls.Add(this.btn_Compute);
             this.Controls.Add(this.cbOperation);
